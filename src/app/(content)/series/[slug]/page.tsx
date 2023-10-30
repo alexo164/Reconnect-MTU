075f987e661ecb53516d150fac9d762e51594142
Alex Oltean <alexoltean164@gmail.com>
2023-10-30 14:32:54 +0000

    more import fixes

--- src/app/(content)/series/[slug]/page.tsx
+++ src/app/(content)/series/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import { Text, Breadcrumbs } from "@/components";
-import { HomeArticles } from "@/containers";
 import {slugToTitle} from "@/utils/utils";
 import { Metadata } from "next";
 import { SanityDocument } from "@sanity/client";
@@ -36,38 +35,7 @@ const SeriesDetail = async ({ params }: { params: { slug: string } }) => {
     params,
   });
   const title=slugToTitle(params.slug)
-  return (
-    <section className="container px-3 md:pb-20 md:pt-10 pt-20">
-      <div className="mt-19">
-        <Breadcrumbs pageName="Series" pageSlug={title} pageLink="/series" />
-        
-        {
-          !articles ? (
-            <p>No Article Found</p>
-          ) : (<>  <Text
-            title
-            className="mb-8 mt-10 dark:text-appRed-100 text-appPurple-100 
-            capitalize"
-          >
-            
-            {title}
-          </Text>
-          <div className={"flex flex-wrap flex-col"}>
-            
-             {articles ? (
-                          <HomeArticles
-                          isArchive={false} noOfArticle={9} articles={articles}
-                          isSeries={false} isExternal={false}
-                          />
-                      ) : (
-                          <p>No Article Found</p>
-                      )}
-          </div></>)
-        }
-       
-      </div>
-    </section>
-  );
+  
 };
 
 export default SeriesDetail;
