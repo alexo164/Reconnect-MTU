ea1a3dd1c4bac04e6f5cb5c54ac8071f381160df
Alex Oltean <alexoltean164@gmail.com>
2023-10-30 14:24:49 +0000

    fix homearticle build error in slug/page

--- src/app/(content)/categories/[slug]/page.tsx
+++ src/app/(content)/categories/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import { Text, Breadcrumbs } from "@/components";
-import { HomeArticles } from "@/containers";
 import { slugToTitle } from "@/utils/utils";
 import { Metadata } from "next";
 import { SanityDocument } from "@sanity/client";
@@ -35,37 +34,7 @@ const CategoryDetail = async ({ params }: { params: { slug: string } }) => {
         params,
     });
     const title = slugToTitle(params.slug);
-    return (
-        <section className='container px-3 pt-20 md:pb-20 md:pt-10'>
-            <div className='mt-19'>
-                <Breadcrumbs
-                    pageName='Categories'
-                    pageSlug={title}
-                    pageLink='/categories'
-                />
-                <Text
-                    title
-                    className='mb-8 mt-10 capitalize text-appPurple-100 
-          dark:text-appRed-100'
-                >
-                    {title}
-                </Text>
-                <div className={"flex flex-col flex-wrap"}>
-                    {articles?.length > 0 ? (
-                        <HomeArticles
-                            isArchive={false}
-                            noOfArticle={9}
-                            articles={articles}
-                            isSeries={false}
-                            isExternal={false}
-                        />
-                    ) : (
-                        <h1>No Articles Found </h1>
-                    )}
-                </div>
-            </div>
-        </section>
-    );
+    
 };
 
 export default CategoryDetail;
